68e4696da6b7e90adef0f54760998e14ce26612f
Mulei Mao <mulei.mao3@gmail.com>
2021-06-26 16:41:31 -0400

    enhanced file

--- TestPythonFile.py
+++ TestPythonFile.py
@@ -18,7 +18,8 @@ arrQ = ["What is your learning style?",    #array to store question information
         "Would you like to work in a specific area or do you have a broad range of interests?",
         "Medicine?",
         "Do you want to work in artistic design applications to engineering?",
-        "Do you enjoy research and analysis or do you prefer a wider range of applications?"
+        "Do you enjoy research and analysis or do you prefer a wider range of applications?",
+        "Not the program for you? Take the quiz again!"
          ]
 
 arrA = ["Gimme the hands on stuff",   #array to store option A information
@@ -40,7 +41,8 @@ arrA = ["Gimme the hands on stuff",   #array to store option A information
         "Broader the better",
         "I need medicine",
         "Artistic design suits me",
-        "Research sounds awesome"
+        "Research sounds awesome",
+        "Yesss, let's go again"
         ]
         
 arrB = ["Gimme the hands off stuff",  #array to store option B information
@@ -62,7 +64,9 @@ arrB = ["Gimme the hands off stuff",  #array to store option B information
         "Specializing is my specialty",
         "I no like medicine",
         "No artistic design for me",
-        "I want a wider range of applications"]
+        "I want a wider range of applications",
+        "Nah, I'm satisfied."
+        ]
 arrProgram = ["You belong in Geological Engineering.",      #array to store program information
               "You belong in Environmental Engineering.",
               "You belong in Architectural Engineering",
@@ -98,133 +102,177 @@ def getMCPick():                 #procedure to track user selection
             print ("Please only enter A or B")  #prints error prompt
     return choice                               #returns standardized choice
 
+def printSpace ():
+    for counter in range(0, 3):
+        print ("")
+
 #obtain user info
 userName = str(input("Please enter your name: "))                    #asks for user name
 print ("Welcome,", userName, "to the Engineering Program selector.") #prints name in a welcome message
+printSpace ()
 
 #error checking student Grade
 while True:
     try:
         userGrade = int(input("Please enter your Grade as a number.\n"
         + "If you are in university, please enter 0: "))
+        printSpace ()
         break
     except: 
         print ("Please enter a number.")
+        printSpace ()
 if userGrade <= 12 and userGrade >= 1:
     print ("Great job of researching your post-secondary options!")
 elif userGrade == 0:
     print ("Why are you doing this quiz if you already have a major? Get a life!")
+    printSpace ()
 
-printQuestion(arrQ[0],arrA[0],arrB[0])
-userInput = getMCPick()
+userExit = True
 
-#branch 1
-if userInput == 'A': #hands on
-    printQuestion(arrQ[1],arrA[1],arrB[1])
+while userExit:
+    printQuestion(arrQ[0],arrA[0],arrB[0])
     userInput = getMCPick()
-    if userInput == 'A': #love nature
-        printQuestion(arrQ[2],arrA[2],arrB[2])
+    printSpace ()
+
+    #branch 1
+    if userInput == 'A': #hands on
+        printQuestion(arrQ[1],arrA[1],arrB[1])
         userInput = getMCPick()
-        if userInput == 'A': #love rocks:
-            print (arrProgram[0])
-        else: #no rocks
-            print (arrProgram[1])
-    else: #no like nature
-        printQuestion (arrQ[3], arrA[3],arrB[3])
-        userInput = getMCPick()
-        if userInput == 'A': #love buildings
-            printQuestion (arrQ[4], arrA[4],arrB[4])
+        printSpace ()
+        if userInput == 'A': #love nature
+            printQuestion(arrQ[2],arrA[2],arrB[2])
             userInput = getMCPick()
-            if userInput == 'A':
-                print (arrProgram[2])
+            printSpace ()
+            if userInput == 'A': #love rocks:
+                print (arrProgram[0])
+            else: #no rocks
+                print (arrProgram[1]) 
+        else: #no like nature
+            printQuestion (arrQ[3], arrA[3],arrB[3])
+            userInput = getMCPick()
+            printSpace ()
+            if userInput == 'A': #love buildings
+                printQuestion (arrQ[4], arrA[4],arrB[4])
+                userInput = getMCPick()
+                printSpace ()
+                if userInput == 'A':
+                    print (arrProgram[2])
+                else:
+                    print (arrProgram[3])
+            else: #no like buildings
+                printQuestion (arrQ[5], arrA[5],arrB[5])
+                userInput = getMCPick()
+                printSpace ()
+                if userInput == 'A': #yes lab
+                    printQuestion (arrQ[6], arrA[6],arrB[6])
+                    userInput = getMCPick()
+                    printSpace ()
+                    if userInput == "A": #yes medicine
+                        print (arrProgram[7])
+                    else:
+                        printQuestion (arrQ[7], arrA[7],arrB[7])
+                        userInput = getMCPick()
+                        printSpace ()
+                        if userInput == 'A': #broad
+                            print (arrProgram[11])
+                        else:
+                            print (arrProgram[10])
+                else: #no lab
+                    printQuestion (arrQ[8], arrA[8],arrB[8])
+                    userInput = getMCPick()
+                    printSpace ()
+                    if userInput == 'A': 
+                        printQuestion (arrQ[9], arrA[9],arrB[9])
+                        userInput = getMCPick()
+                        printSpace ()
+                        if userInput == 'A':
+                            print (arrProgram[8])
+                        else:
+                            print (arrProgram[9])
+                    else:
+                        printQuestion (arrQ[10], arrA[10],arrB[10])
+                        userInput = getMCPick()
+                        printSpace ()
+                        if userInput == 'A': #hardware
+                            printQuestion (arrQ[11], arrA[11],arrB[11])
+                            userInput = getMCPick()
+                            printSpace ()
+                            if userInput == 'A': #power
+                                print (arrProgram[4])
+                            else: #computer
+                                print (arrProgram[5])
+                        else: #broad
+                            printQuestion (arrQ[12], arrA[12],arrB[12])
+                            userInput = getMCPick()
+                            printSpace ()
+                            if userInput == 'A': #syde
+                                print (arrProgram[6])
+                            else: #biomed
+                                print (arrProgram[7])
+    else: #hands off
+        printQuestion(arrQ[1],arrA[1],arrB[1])
+        userInput = getMCPick()
+        printSpace ()
+        if userInput == 'A': #love nature
+            printQuestion(arrQ[2],arrA[2],arrB[2])
+            userInput = getMCPick()
+            printSpace ()
+            if userInput == 'A': #love rocks:
+                print (arrProgram[0])
+            else: #no rocks
+                print (arrProgram[1])
+        else:                    
+            printQuestion(arrQ[13],arrA[13],arrB[13])
+            userInput = getMCPick()
+            printSpace ()
+            if userInput == 'A': #yes business
+                print (arrProgram[13])
             else:
-                print (arrProgram[3])
-        else: #no like buildings
-            printQuestion (arrQ[5], arrA[5],arrB[5])
-            userInput = getMCPick()
-            if userInput == 'A': #yes lab
-                printQuestion (arrQ[6], arrA[6],arrB[6])
+                printQuestion(arrQ[14],arrA[14],arrB[14])
                 userInput = getMCPick()
-                if userInput == "A": #yes medicine
-                    print (arrProgram[7])
-                else:
-                    printQuestion (arrQ[7], arrA[7],arrB[7])
+                printSpace ()
+                if userInput == 'A': #yes computers 
+                    printQuestion(arrQ[15],arrA[15],arrB[15])
                     userInput = getMCPick()
-                    if userInput == 'A': #broad
-                        print (arrProgram[11])            
-                    else:
-                        print (arrProgram[10])
-            else: #no lab
-                printQuestion (arrQ[8], arrA[8],arrB[8])
-                userInput = getMCPick()
-                if userInput == 'A': 
-                    printQuestion (arrQ[9], arrA[9],arrB[9])
-                    userInput = getMCPick()
-                    if userInput == 'A':
-                        print (arrProgram[8])
-                    else:
-                        print (arrProgram[9])
-                else:
-                    printQuestion (arrQ[10], arrA[10],arrB[10])
-                    userInput = getMCPick()
+                    printSpace ()
                     if userInput == 'A': #hardware
-                        printQuestion (arrQ[11], arrA[11],arrB[11])
-                        userInput = getMCPick()
-                        if userInput == 'A': #power
-                            print (arrProgram[4])
-                        else: #computer
-                            print (arrProgram[5])
-                    else: #broad
-                        printQuestion (arrQ[12], arrA[12],arrB[12])
-                        userInput = getMCPick()
-                        if userInput == 'A': #syde
-                            print (arrProgram[6])
-                        else: #biomed
-                            print (arrProgram[7])
-else: #hands off
-    printQuestion(arrQ[1],arrA[1],arrB[1])
-    userInput = getMCPick()
-    if userInput == 'A': #love nature
-        printQuestion(arrQ[2],arrA[2],arrB[2])
-        userInput = getMCPick()
-        if userInput == 'A': #love rocks:
-            print (arrProgram[0])
-        else: #no rocks
-            print (arrProgram[1])
-    else:                    
-        printQuestion(arrQ[13],arrA[13],arrB[13])
-        userInput = getMCPick()
-        if userInput == 'A': #yes business
-            print (arrProgram[13])
-        else:
-            printQuestion(arrQ[14],arrA[14],arrB[14])
-            userInput = getMCPick()
-            if userInput == 'A': #yes computers 
-                printQuestion(arrQ[15],arrA[15],arrB[15])
-                userInput = getMCPick()
-                if userInput == 'A': #hardware
-                    print (arrProgram[5])
-                else: 
-                    print (arrProgram[12])
-            else:
-                printQuestion(arrQ[16],arrA[16],arrB[16])
-                userInput = getMCPick()
-                if userInput == 'A': #broad
+                        print (arrProgram[5])
+                    else: 
+                        print (arrProgram[12])
+                else:
                     printQuestion(arrQ[16],arrA[16],arrB[16])
                     userInput = getMCPick()
-                    if userInput == 'A': #yes medicine
-                        print (arrProgram[7])
+                    printSpace ()
+                    if userInput == 'A': #broad
+                        printQuestion(arrQ[17],arrA[17],arrB[17])
+                        userInput = getMCPick()
+                        printSpace ()
+                        if userInput == 'A': #yes medicine
+                            print (arrProgram[7])
+                        else: 
+                            print (arrProgram[6])
                     else: 
-                        print (arrProgram[6])
-                else: 
-                    printQuestion(arrQ[17],arrA[17],arrB[17])
-                    userInput = getMCPick()
-                    if userInput == 'A': #yes artistic design
-                        print (arrProgram[2])
-                    else:
                         printQuestion(arrQ[18],arrA[18],arrB[18])
                         userInput = getMCPick()
-                        if userInput == 'A': #research
-                            print (arrProgram[10])
+                        printSpace ()
+                        if userInput == 'A': #yes artistic design
+                            print (arrProgram[2])
                         else:
-                            print (arrProgram[11])
+                            printQuestion(arrQ[19],arrA[19],arrB[19])
+                            userInput = getMCPick()
+                            printSpace ()
+                            if userInput == 'A': #research
+                                print (arrProgram[10])
+                            else:
+                                print (arrProgram[11])
+    
+    printQuestion(arrQ[20],arrA[20],arrB[20])
+    userInput = getMCPick()
+    printSpace ()
+    if userInput == 'A': #encore
+        print ("Reinitiating the quiz...")    
+        printSpace ()
+    else:
+        print ("Have a nice day!")
+        userExit = False
+
